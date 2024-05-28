eeff973c94f4bcc1dfc789b142b1fe83516d8b17
Davismeru <davismeru911@gmail.com>
2024-05-28 16:50:45 +0300

    added password hashing with bcrypt

--- server/controllers/customer_signup_controller.js
+++ server/controllers/customer_signup_controller.js
@@ -1,14 +1,22 @@
+const bcrypt = require("bcryptjs");
+const saltRounds = 10;
+
 const customer_signup = require("../models/customer_signup");
 
 const customerSignupController = async (req, res) => {
   const { phone_number, username, email, password } = req.body;
-  const customerDetails = await customer_signup.create({
-    phone_number,
-    username,
-    email,
-    password,
+
+  // password hashing
+  bcrypt.hash(password, saltRounds).then(function (hash) {
+    const customerDetails = customer_signup.create({
+      phone_number,
+      username,
+      email,
+      password: hash,
+    });
+
+    res.json(customerDetails);
   });
-  res.json(customerDetails);
 };
 
 module.exports = {
